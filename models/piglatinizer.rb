3a958bd8c893ef89766026fbeed47fab1060f8c9
Min Kim <mk4555@nyu.edu>
2018-01-13 22:38:34 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def piglatinize(words)
+    vowels = %w{a e i o u}
+    arr = words.split(" ")
+    result = []
+    arr.each do |word|
+      if vowels.include?(word[0].downcase)
+        result << word + "way"
+        return result.join(" ")
+      else
+        letters = word.split("")
+        counter = 0
+      end
+        # result << word[1..-1] + word[0] + "ay"
+
+    # result.join(" ")
+  end
+
+end
